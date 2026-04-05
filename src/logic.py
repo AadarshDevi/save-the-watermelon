f3153eecacba77c1f2a0ad40c97acdbd776b8c68
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-04-05 10:54:22 -0700

    created __init__ that will create the vars (private) needed

--- src/logic.py
+++ src/logic.py
@@ -1,1 +1,9 @@
+    def __init__(self, slices, word):
+        self.__slices: int = slices
+        self.__word: list = list(word)
+        self.__visual_word: list = ["_"] * len(self.__word)
+        self.__visible_letters: int = 0
+        self.__letters_entered: list = []
+        pass
+
 
