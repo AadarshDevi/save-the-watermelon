7319294cc369c02358e061ccd42eb2f23cd181f8
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-04-05 11:33:46 -0700

    created the words loader class by copy pasting the and replacing CLASS, FUNCTION, IF, etc by the correct python syntax

--- src/words.py
+++ src/words.py
@@ -1,1 +1,26 @@
 
+class WordLoader
+
+def __init__(self, slices):
+    set __words_list as empty list
+
+
+def read_words(filepath)
+    reset __words_list by making it empty
+    get file and read it line by line
+    if word is not empty
+    add it to a list
+
+
+    close file
+    give the words_in_file list
+
+
+def get_new_word
+    get a psuedo random int in range of 0 to length of __words_list
+    get a word from __words_list at the index of the pseudo random int
+    increment the __word_index
+    if word is empty
+    give None
+
+    give the word
