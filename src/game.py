10f6822875b32245debc43c3288f05b58f3b582d
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-04-05 18:17:43 -0700

    word loader object created and reads data from the specific words file

--- src/game.py
+++ src/game.py
@@ -4,6 +4,9 @@ from src.words import WordLoader
 
 
 def main_game_loop():
+    wl: WordLoader = WordLoader()
+    wl.read_words("../data/words.txt")
+
     slices = 6
     wl: WordLoader = WordLoader
     gl: GameLogic = GameLogic()
